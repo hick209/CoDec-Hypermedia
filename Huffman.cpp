d0933c7197ff738a5d9bec08c261a94f0f7ba2d1
LarissaCardoso <larissacazi@hotmail.com>
2014-11-11 22:36:25 -0200

    X

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,9 +1,18 @@
 #include "Huffman.h"
-
 #include <stdio.h>
 #include "Utils.h"
 #include "WaveReader.h"
 
+
+//Return the frenquency in order of each caracter
+void frequencyInOrder(){
+	std::vector
+	
+}
+
+void buildTree(){
+
+}
 
 void huffmanCompress(const char* filename, const char* filenameExtensionLess) {
 	WaveReader reader(filename);
