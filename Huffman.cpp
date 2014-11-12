2d57a563cbe8f0b0ae0d3f920e37588254de8c62
LarissaCardoso <larissacazi@hotmail.com>
2014-11-12 00:10:26 -0200

    Y

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,24 +1,29 @@
-#include "Huffman.h"
+#include "FUF.h"
 #include <stdio.h>
 #include "Utils.h"
 #include "WaveReader.h"
 
 using namespace std;
 
-//Return the frenquency in order of each caracter
-int** frequencyInOrder(WaveData sample){
+//Return the frenquency in order of each channel
+int** frequencyInOrder(int* channel, int dataLength){
 	vector< vector<int> > frequency;
+	int i = 0, aux, count = 0;
 	
+	aux = channel(0);
 	
+	for(i=0;i<dataLength;i++) {
+			
+	}
 	
 } 
 
-void buildTree(){
-
+void buildTree(WaveReader sample){
+	vector< 
 }
 
 void FUF::huffmanCompress() {
-	frequencyInOrder(sample.data);
+	buildTree(sample);
 	printWavInfo(sample.info);
 }
 
