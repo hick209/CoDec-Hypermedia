3cf242a87a64e982ca3f96c8ad39805e87751742
rvferreira <raphael.rvf@gmail.com>
2014-11-18 15:19:10 -0200

    compression modularized

--- FUF.cpp
+++ FUF.cpp
@@ -1,7 +1,34 @@
 #include "FUF.h"
+#include <algorithm>
 
-FUF::FUF(const char* filename, const char* filenameExtensionLess) : sample(filename) {
+FUF::FUF(const char* filename, const char* filenameExtensionLess)
+		: sample(filename), lastCompression(0){
+	for (int i = 0, i < COMPRESSION_MODE_COUNT; i++){
+		compress[i] = NONE;
+	}
 	sample.readWav();
 }
 
-FUF::~FUF(){ }
+FUF::~FUF(){}
+
+FUF::compress(compressMode a){
+	for (int i = 0, i < lastCompression; i++){
+		if (compress[i] == a) {
+			cout << "This compression was already applied. YOCO, bro!";
+			return;
+		}
+	}
+	compress[lastCompression] = a;
+	lastCompression++;
+}
+
+FUF::compress(compressMode a, compressMode b){
+	compress(a);
+	compress(b);
+}
+
+FUF::compress(compressMode a, compressMode b, compressMode c){
+	compress(a);
+	compress(b);
+	compress(c);
+}
--- FUF.h
+++ FUF.h
@@ -1,22 +1,31 @@
 #ifndef FUF_H
 #define FUF_H
 
+#define COMPRESSION_MODE_COUNT 3
+
 #include <stdio.h>
 #include "WaveReader.h"
-
 
 class FUF {
 private:
 	WaveReader sample;
-	int huffmanized;
-	int differentiated;
-	int transformed;
+	
+	enum {
+		NONE = 0,
+		HUFFMAN = 1,
+		DIFFERENCE = 2,
+		TRANSFORM = 3
+	} compression[COMPRESSION_MODE_COUNT];
+	int lastCompression;
 
 public:
 
 	FUF(const char* filename, const char* filenameExtensionLess);
 	~FUF();
 
+	void compress(compressMode);
+	void compress(compressMode, compressMode);
+	void compress(compressMode, compressMode, compressMode);
 	void huffmanCompress();
 	void differencialCompress();
 	void transformCompress();
