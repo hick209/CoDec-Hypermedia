b3ac6e8dbb37a9904babf2688ec8fd7d05b1d6fc
LarissaCardoso <larissacazi@hotmail.com>
2014-11-19 21:12:45 -0200

    Atualizacao.

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,22 +1,30 @@
 #include "FUF.h"
 #include <stdio.h>
+#include <vector>
 #include "Utils.h"
 #include "WaveReader.h"
 
 using namespace std;
-
+/*
 //Return the frenquency in order of each channel
 int** frequencyInOrder(int* channel, int dataLength){
 	vector< vector<int> > frequency;
-	int i = 0, aux, count = 0;
+	int i = 0, j = 0, aux, count = 0;
 	
-	aux = channel(0);
-	
-	for(i=0;i<dataLength;i++) {
-				if(aux == channel(i)) count++;
+	while( j < dataLength ) {
+		aux = channel(j);
+		for(i=0;i<dataLength;i++) {
+					if(aux == channel(i)) count++;
+		}
+		if(frenquency.size == 0){ 
+			frequency(j)(0).push_back(aux);
+			frequency(j)(1).push_back(count);
+		}
+		else {
+		}
+		j++;
 	}
-	
-} 
+} */
 
 void buildTree(WaveReader sample){
  
